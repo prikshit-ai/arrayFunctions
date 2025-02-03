12c44b8803cad36dfa3a6bfe4d8f7b06dc46864a
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-02-03 19:52:50 +0530

    userinput

--- UserInput.java
+++ UserInput.java
@@ -1,5 +1,16 @@
-package javalab.ArrayFunction;
-
+import java.util.*;
 public class UserInput {
+    public int[] arrInput(){
+        Scanner cf = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.println("enter 5 numbers :");
+        for (int i = 0; i < 5; i++) {
+            array[i] = cf.nextInt();
+            
+        }
+        return array ;
+
+
+    }
     
 }
