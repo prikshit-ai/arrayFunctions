34b4765dbc631ca680e9432018f538f0e0bb4252
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-02-03 19:53:47 +0530

    even odd

--- ArrayFunction.java
+++ ArrayFunction.java
@@ -1,5 +1,28 @@
-package javalab.ArrayFunction;
+import java.util.ArrayList;
 
 public class ArrayFunction {
+
     
-}
+    public void display(ArrayList<Integer> array) {
+        System.out.println("Array is: " + array);
+    }
+
+    
+    public void oddEven(int[] array) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        
+        for (int num : array) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        
+        System.out.println("Even numbers:");
+        display(even);
+
+        System.out.println("Odd numbers:");
+        display(odd);
+    }
