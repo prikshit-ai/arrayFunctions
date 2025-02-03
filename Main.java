c2b315db7ddc142eb4b4d73ca94401a526b68deb
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-02-03 20:01:36 +0530

    Main

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-package javalab.ArrayFunction;
-
 public class Main {
+    public static void main(String[] args) {
+        UserInput array = new UserInput();
+       /*  ArrayFunctions choice = new ArrayFunctions();
+        choice.oddEven(array.arrInput());
+*/
+        ArrayFunction choice = new ArrayFunction();
+        choice.consecDiff(array.arrInput());
+        
+    }
+
     
 }
